b8b8d7cdfd8173a8980f3ff7e67a4f62150266c6
markgenuine <crazygroup.corp@gmail.com>
2022-03-04 00:32:50 +0300

    feat: add new errors

--- contracts/libraries/SwapEverErrors.sol
+++ contracts/libraries/SwapEverErrors.sol
@@ -1,8 +1,9 @@
 pragma ton-solidity >= 0.57.0;
 
 library SwapEverErrors {
-    uint16 constant NOT_ROOT_WEVER   = 100;
-    uint16 constant NOT_WALLET_WEVER = 101;
-    uint16 constant INVALID_CALLBACK = 102;
-    uint16 constant VALUE_TOO_LOW    = 103;
+    uint16 constant NOT_ROOT_WEVER           = 100;
+    uint16 constant NOT_WALLET_WEVER         = 101;
+    uint16 constant INVALID_CALLBACK         = 102;
+    uint16 constant VALUE_TOO_LOW            = 103;
+    uint16 constant UNKNOWN_OPERATION_STATUS = 104;
 }
