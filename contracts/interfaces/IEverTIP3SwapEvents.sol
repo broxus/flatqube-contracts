3b74252490cf3946a905de02f6cca00a3022c435
markgenuine <crazygroup.corp@gmail.com>
2022-03-07 00:42:38 +0300

    feat: added new event

--- contracts/interfaces/IEverTIP3SwapEvents.sol
+++ contracts/interfaces/IEverTIP3SwapEvents.sol
@@ -2,6 +2,7 @@ pragma ton-solidity >= 0.57.0;
 
 interface IEverTIP3SwapEvents {
     event WrapEverToWEver(address user, uint64 id);
+    event UnwrapWEverToEver(address user, uint64 id);
     event WEverTokenMint(address user, uint64 id);
     event WEverTIP3Cancel(address user, uint64 id);
     event TIP3WEverCancel(address user, uint64 id);
