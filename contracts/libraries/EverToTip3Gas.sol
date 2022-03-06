0d9220a9118a6259764cb734c25a784e7e96bb5e
markgenuine <crazygroup.corp@gmail.com>
2022-03-06 17:31:03 +0300

    fix: error symbol in name

--- contracts/libraries/EverToTip3Gas.sol
+++ contracts/libraries/EverToTip3Gas.sol
@@ -4,6 +4,6 @@ library EverToTip3Gas {
     uint128 constant ACCOUNT_INITIAL_BALANCE        = 1 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_VALUE      = 1 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_GRAMS      = 1 ton;
-    uint128 constant GAS_SWAP§                      = 5 ton;
+    uint128 constant GAS_SWAP                      = 5 ton;
 
 }
