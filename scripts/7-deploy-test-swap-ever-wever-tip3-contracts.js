1c79196234e5a5a4a85a1beb49c635a0d949517d
markgenuine <crazygroup.corp@gmail.com>
2022-03-13 02:31:38 +0300

    feat: add tests and code style

--- scripts/7-deploy-test-swap-ever-wever-tip3-contracts.js
+++ scripts/7-deploy-test-swap-ever-wever-tip3-contracts.js
@@ -1,4 +1,4 @@
-const {getRandomNonce, Migration, WEVER_CONTRACTS_PATH, TOKEN_CONTRACTS_PATH} = require(process.cwd()+'/scripts/utils')
+const {getRandomNonce, Migration, TOKEN_CONTRACTS_PATH} = require(process.cwd()+'/scripts/utils')
 const {Command} = require('commander');
 const program = new Command();
 
@@ -6,20 +6,20 @@ async function main() {
     const migration = new Migration();
 
     const EverToTip3 = await locklift.factory.getContract('EverToTip3');
-    const Tip3ToEver = await locklift.factory.getContract('TIP3ToEver');
-    const EverWEverToTIP3 = await locklift.factory.getContract('EverWEverToTIP3');
+    const Tip3ToEver = await locklift.factory.getContract('Tip3ToEver');
+    const EverWEverToTip3 = await locklift.factory.getContract('EverWEverToTip3');
 
     const [keyPair] = await locklift.keys.getKeyPairs();
 
-    const WEVERRoot = migration.load(await locklift.factory.getContract('TokenRootUpgradeable', TOKEN_CONTRACTS_PATH), 'WEVERRoot').address;
-    const WEVERVault = migration.load(await locklift.factory.getContract('TestWeverVault'), 'WEVERVault').address;
+    const wEverRoot = migration.load(await locklift.factory.getContract('TokenRootUpgradeable', TOKEN_CONTRACTS_PATH), 'WEVERRoot').address;
+    const wEverVault = migration.load(await locklift.factory.getContract('TestWeverVault'), 'WEVERVault').address;
 
-    console.log(`Deploying EverToTip3...`);
+    console.log(`Deploying EverToTip3 contract...`);
     const everToTip3 = await locklift.giver.deployContract({
         contract: EverToTip3,
         constructorParams: {
-            _wEverRoot: WEVERRoot,
-            _wEverVault: WEVERVault
+            _wEverRoot: wEverRoot,
+            _wEverVault: wEverVault
         },
         initParams: {
             randomNonce_: getRandomNonce(),
@@ -27,14 +27,14 @@ async function main() {
         },
         keyPair,
     }, locklift.utils.convertCrystal('2', 'nano'));    
-    console.log(`EverToTip3 address: ${everToTip3.address}`);
+    console.log(`EverToTip3 deploing end. Address: ${everToTip3.address}`);
 
     console.log(`Deploying Tip3ToEver...`);
     const tip3ToEver = await locklift.giver.deployContract({
         contract: Tip3ToEver,
         constructorParams: {
-            _wEverRoot: WEVERRoot,
-            _wEverVault: WEVERVault,
+            _wEverRoot: wEverRoot,
+            _wEverVault: wEverVault,
         },
         initParams: {
             randomNonce_: getRandomNonce(),
@@ -42,14 +42,14 @@ async function main() {
         },
         keyPair,
     }, locklift.utils.convertCrystal('2', 'nano'));    
-    console.log(`Tip3ToEver address: ${tip3ToEver.address}`);
+    console.log(`Tip3ToEver deploing end. Address: ${tip3ToEver.address}`);
 
-    console.log(`Deploying EverWEverToTIP3...`);
+    console.log(`Deploying EverWEverToTip3...`);
     const everWEverToTIP3 = await locklift.giver.deployContract({
-        contract: EverWEverToTIP3,
+        contract: EverWEverToTip3,
         constructorParams: {
-            _wEverRoot: WEVERRoot,
-            _wEverVault: WEVERVault,
+            _wEverRoot: wEverRoot,
+            _wEverVault: wEverVault,
             _swapEver: everToTip3.address,
         },
         initParams: {
@@ -58,11 +58,11 @@ async function main() {
         },
         keyPair,
     }, locklift.utils.convertCrystal('2', 'nano'));    
-    console.log(`EverWEverToTIP3 address: ${everWEverToTIP3.address}`);
+    console.log(`EverWEverToTip3 deploing end. Address: ${everWEverToTIP3.address}`);
 
     migration.store(everToTip3, 'EverToTip3');
     migration.store(tip3ToEver, 'Tip3ToEver');
-    migration.store(everWEverToTIP3, 'EverWEverToTIP3');
+    migration.store(everWEverToTIP3, 'EverWEverToTip3');
 }
 
 main()
