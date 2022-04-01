12a289cc80862c822dbf49040af9e83fa2cad1fc
markgenuine <crazygroup.corp@gmail.com>
2022-04-01 21:01:31 +0300

    upd: change gas for deploy empty wallet

--- contracts/libraries/LimitOrdersGas.sol
+++ contracts/libraries/LimitOrdersGas.sol
@@ -5,7 +5,7 @@ library LimitOrdersGas {
     uint128 constant SET_CODE                       = 0.1 ton;
     uint128 constant DEPLOY_ORDERS_ROOT             = 2 ton;
     uint128 constant DEPLOY_ORDER_MIN_VALUE         = 3 ton;
-    uint128 constant DEPLOY_EMPTY_WALLET_VALUE      = 0.5 ton;
+    uint128 constant DEPLOY_EMPTY_WALLET_VALUE      = 0.2 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_GRAMS      = 0.1 ton;
     uint128 constant SWAP_MIN_VALUE                 = 3 ton;
 }
