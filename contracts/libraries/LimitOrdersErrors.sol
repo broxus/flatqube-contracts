eeb7b518e66a761c235dff186d79caacf4fa903f
markgenuine <crazygroup.corp@gmail.com>
2022-04-05 22:44:53 +0300

    feat: change errors

--- contracts/libraries/LimitOrdersErrors.sol
+++ contracts/libraries/LimitOrdersErrors.sol
@@ -5,9 +5,9 @@ library LimitOrdersErrors {
     uint16 constant NOT_PENDING_OWNER      = 301;
     uint16 constant VALUE_TOO_LOW          = 302;
     uint16 constant EMPTY_SALT_IN_ORDER    = 303;
-    uint16 constant NOT_DEX_ROOT           = 304;
+    uint16 constant NOT_BEGIN_DATA         = 304;
     uint16 constant NOT_LIMITS_ORDER_OWNER = 305;
-    uint16 constant NOT_TOKEN1_ROOT        = 306;
-    uint16 constant NOT_TOKEN2_ROOT        = 307;
+    uint16 constant NOT_WALLET_TOKEN_2     = 306;
+    uint16 constant NOT_ACTIVE_LIMIT_ORDER = 307;
     uint16 constant NOT_BACKEND_PUB_KEY    = 308;
 }
