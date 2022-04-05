7d275dc293cfc98df245a75062ef9880ffc07bdd
markgenuine <crazygroup.corp@gmail.com>
2022-04-05 22:45:28 +0300

    feat: changes gas

--- contracts/libraries/LimitOrdersGas.sol
+++ contracts/libraries/LimitOrdersGas.sol
@@ -7,5 +7,6 @@ library LimitOrdersGas {
     uint128 constant DEPLOY_ORDER_MIN_VALUE         = 3 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_VALUE      = 0.2 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_GRAMS      = 0.1 ton;
-    uint128 constant SWAP_MIN_VALUE                 = 3 ton;
+    uint128 constant SWAP_BACK_MIN_VALUE            = 3 ton;
+    uint128 constant SWAP_MIN_VALUE                 = 5 ton;
 }
