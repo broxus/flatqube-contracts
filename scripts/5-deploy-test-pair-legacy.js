c76c6090882238118d207c3d4bb1aa68e530f316
Елена Яковлева <el.iakovv@gmail.com>
2023-02-02 19:54:47 +0100

    fix only-upgrade test for prev contracts

---
+++ scripts/5-deploy-test-pair-legacy.js
@@ -0,0 +1,155 @@
+const {Migration, TOKEN_CONTRACTS_PATH, Constants, afterRun, displayTx} = require(process.cwd()+'/scripts/utils')
+const { Command } = require('commander');
+const program = new Command();
+
+async function main() {
+  console.log('5-deploy-test-pair.js');
+  const migration = new Migration();
+  const keyPairs = await locklift.keys.getKeyPairs();
+
+  const account2 = migration.load(await locklift.factory.getAccount('Wallet'), 'Account2');
+
+  if (locklift.tracing) {
+    locklift.tracing.allowCodes({compute: [100]});
+  }
+
+  const dexVault = migration.load(await locklift.factory.getContract('DexVault'), 'DexVault');
+  const dexRoot = migration.load(await locklift.factory.getContract('DexRoot'), 'DexRoot');
+
+  account2.afterRun = afterRun;
+
+  program
+      .allowUnknownOption()
+      .option('-p, --pairs <pairs>', 'pairs to deploy')
+      .option('-cn, --contract_name <contract_name>', 'New version of contract name');
+
+  program.parse(process.argv);
+
+  const options = program.opts();
+  options.contract_name = options.contract_name || 'DexPair';
+
+  const pairs = options.pairs ? JSON.parse(options.pairs) : [['foo', 'bar']];
+
+  for (const p of pairs) {
+
+    const tokenLeft = p[0].slice(-2) === 'Lp' ? {name: p[0], symbol: p[0], decimals: Constants.LP_DECIMALS, upgradeable: true} : Constants.tokens[p[0]];
+    const tokenRight = p[1].slice(-2) === 'Lp' ? {name: [p[1]], symbol: p[1], decimals: Constants.LP_DECIMALS, upgradeable: true} : Constants.tokens[p[1]];
+
+    const pair = {left: tokenLeft.symbol, right: tokenRight.symbol};
+
+    console.log(`Start deploy pair DexPair${pair.left}${pair.right}`);
+
+    const tokenFoo = migration.load(await locklift.factory.getContract(
+        tokenLeft.upgradeable ? 'TokenRootUpgradeable' : 'TokenRoot',
+        TOKEN_CONTRACTS_PATH
+    ), pair.left + 'Root');
+    const tokenBar = migration.load(await locklift.factory.getContract(
+        tokenRight.upgradeable ? 'TokenRootUpgradeable' : 'TokenRoot',
+        TOKEN_CONTRACTS_PATH
+    ), pair.right + 'Root');
+
+    const tx = await account2.runTarget({
+      contract: dexRoot,
+      method: 'deployPair',
+      params: {
+        left_root: tokenFoo.address,
+        right_root: tokenBar.address,
+        send_gas_to: account2.address,
+      },
+      value: locklift.utils.convertCrystal(10, 'nano'),
+      keyPair: keyPairs[1]
+    });
+
+    displayTx(tx);
+
+    await afterRun();
+
+    const dexPairFooBarAddress = await dexRoot.call({
+      method: 'getExpectedPairAddress',
+      params: {
+        'left_root': tokenFoo.address,
+        'right_root': tokenBar.address,
+      }
+    })
+
+    console.log(`DexPair${pair.left}${pair.right}: ${dexPairFooBarAddress}`);
+
+    const dexPairFooBar = await locklift.factory.getContract(options.contract_name);
+    dexPairFooBar.address = dexPairFooBarAddress;
+    migration.store(dexPairFooBar, 'DexPool' + pair.left + pair.right);
+
+    const version = await dexPairFooBar.call({method: "getVersion", params: {}})
+    console.log(`DexPair${pair.left}${pair.right} version = ${version}`);
+
+    // await new Promise(resolve => setTimeout(resolve, 10000));
+
+    const active = await dexPairFooBar.call({method: "isActive", params: {}})
+    console.log(`DexPair${pair.left}${pair.right} active = ${active}`);
+
+    const FooBarLpRoot = await locklift.factory.getContract('TokenRootUpgradeable', TOKEN_CONTRACTS_PATH);
+    FooBarLpRoot.setAddress((await dexPairFooBar.call({method: "getTokenRoots"})).lp);
+
+    const FooPairWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    FooPairWallet.setAddress(await tokenFoo.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexPairFooBarAddress,
+      }
+    }));
+
+    const BarPairWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    BarPairWallet.setAddress(await tokenBar.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexPairFooBarAddress,
+      }
+    }));
+
+    const FooBarLpPairWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    FooBarLpPairWallet.setAddress(await FooBarLpRoot.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexPairFooBarAddress,
+      }
+    }));
+
+    const FooVaultWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    FooVaultWallet.setAddress(await tokenFoo.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexVault.address,
+      }
+    }));
+
+    const BarVaultWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    BarVaultWallet.setAddress(await tokenBar.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexVault.address,
+      }
+    }));
+
+    const FooBarLpVaultWallet = await locklift.factory.getContract('TokenWalletUpgradeable', TOKEN_CONTRACTS_PATH);
+    FooBarLpVaultWallet.setAddress(await FooBarLpRoot.call({
+      method: "walletOf",
+      params: {
+        walletOwner: dexVault.address,
+      }
+    }));
+
+    migration.store(FooBarLpRoot, pair.left + pair.right + 'LpRoot');
+    migration.store(FooPairWallet, pair.left + pair.right + 'Pool_' + pair.left + 'Wallet');
+    migration.store(BarPairWallet, pair.left + pair.right + 'Pool_' + pair.right + 'Wallet');
+    migration.store(FooBarLpPairWallet, pair.left + pair.right + 'Pool_LpWallet');
+    migration.store(FooVaultWallet, pair.left + 'VaultWallet');
+    migration.store(BarVaultWallet, pair.right + 'VaultWallet');
+    migration.store(FooBarLpVaultWallet, pair.left + pair.right + 'LpVaultWallet');
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.log(e);
+    process.exit(1);
+  });
--- test_only_upgrade.sh
+++ test_only_upgrade.sh
@@ -11,11 +11,11 @@ npx locklift run $NO_TRACE --script scripts/0-reset-migration.js
 npx locklift run $NO_TRACE --script scripts/0-deploy-account.js --key_number='0' --balance='50'
 npx locklift run $NO_TRACE --script scripts/0-deploy-account.js --key_number='1' --balance='50'
 npx locklift run $NO_TRACE --script scripts/0-deploy-TokenFactory.js
-npx locklift run $NO_TRACE --script scripts/1-deploy-vault-and-root.js --pair_contract_name='DexPairPrev' --root_contract_name='DexRootPrev' --vault_contract_name='DexVaultPrev' --account_contract_name='DexAccountPrev'
+npx locklift run $NO_TRACE --script scripts/1-deploy-vault-and-root-legacy.js --pair_contract_name='DexPairPrev' --root_contract_name='DexRootPrev' --vault_contract_name='DexVaultPrev' --account_contract_name='DexAccountPrev'
 npx locklift run $NO_TRACE --script scripts/2-deploy-test-tokens.js --tokens='["foo","bar","qwe"]'
 npx locklift run $NO_TRACE --script scripts/3-mint-test-tokens.js --mints='[{"account":2,"amount":200000000,"token":"bar"},{"account":2,"amount":200000000,"token":"foo"},{"account":2,"amount":200000000,"token":"qwe"}]'
 npx locklift run $NO_TRACE --script scripts/4-deploy-test-dex-account.js --owner_n=2 --contract_name='DexAccountPrev'
-npx locklift run $NO_TRACE --script scripts/5-deploy-test-pair.js --pairs='[["foo", "bar"], ["bar", "qwe"]]' --contract_name='DexPairPrev'
+npx locklift run $NO_TRACE --script scripts/5-deploy-test-pair-legacy.js --pairs='[["foo", "bar"], ["bar", "qwe"]]' --contract_name='DexPairPrev'
 npx locklift test $NO_TRACE --tests test/30-install-pair-code-v2.js --contract_name='DexStablePair' --pool_type=2
 
 
