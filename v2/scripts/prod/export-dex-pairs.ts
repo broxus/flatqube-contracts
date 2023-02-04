0bef622a41060ddb23696ccf0d544164fe60551e
Alexander <fairyfromalfeya@gmail.com>
2023-02-04 22:15:23 +0100

    fix: eslint, tsconfig, prettier and export pairs script update

--- v2/scripts/prod/export-dex-pairs.ts
+++ v2/scripts/prod/export-dex-pairs.ts
@@ -1,46 +1,87 @@
-// @ts-ignore
-const fs = require('fs');
+import { writeFileSync } from 'fs';
+import { Address } from 'locklift';
+
+const OLD_DEX_PAIR_CODE_HASH =
+  'd7f137ee2123785ed7dd56fad374ab7c0e99343eb97e918aac1bbc6bd9bb827b';
+const DEX_ROOT_ADDRESS =
+  '0:5eb5713ea9b4a0f3a13bc91b282cde809636eb1e68d2fcb6427b9ad78a5a9008';
+
+type PairEntity = {
+  dexPair: Address;
+  left: Address;
+  right: Address;
+  lp: Address;
+};
 
 async function exportDexPairs() {
-  const OLD_DEX_PAIR_CODE_HASH = 'd7f137ee2123785ed7dd56fad374ab7c0e99343eb97e918aac1bbc6bd9bb827b';
-  const DEX_ROOT_ADDRESS = '0:5eb5713ea9b4a0f3a13bc91b282cde809636eb1e68d2fcb6427b9ad78a5a9008';
-
-  let dexPairsToUpdate = [];
   let continuation = undefined;
   let hasResults = true;
+  const accounts: Address[] = [];
+
+  const start = Date.now();
+
   while (hasResults) {
-    let result: any = await locklift.provider.getAccountsByCodeHash({
-          codeHash: OLD_DEX_PAIR_CODE_HASH,
-          continuation,
-          limit: 50
-    });
+    const result: { accounts: Address[]; continuation: string } =
+      await locklift.provider.getAccountsByCodeHash({
+        codeHash: OLD_DEX_PAIR_CODE_HASH,
+        continuation,
+        limit: 50,
+      });
+
     continuation = result.continuation;
     hasResults = result.accounts.length === 50;
-    for (const dexPairAddress of result.accounts) {
-      const DexPair = await locklift.factory.getDeployedContract('DexPair', dexPairAddress);
-      // @ts-ignore
-        const root = (await DexPair.methods.getRoot({ answerId: 0 }).call({})).dex_root.toString();
-      if (root === DEX_ROOT_ADDRESS) {
-        // @ts-ignore
-          const roots = await DexPair.methods.getTokenRoots({ answerId: 0 }).call();
-        console.log(`DexPair ${dexPairAddress}, left = ${roots.left}, right = ${roots.right}, lp = ${roots.lp}`);
-        dexPairsToUpdate.push({
-          dexPair: dexPairAddress,
-          left: roots.left,
-          right: roots.right,
-          lp: roots.lp
-        });
-      }
-    }
-  }
-  fs.writeFileSync('./dex_pairs.json', JSON.stringify(dexPairsToUpdate));
 
+    accounts.push(...result.accounts);
+  }
+
+  const promises: Promise<PairEntity>[] = [];
+
+  for (const dexPairAddress of accounts) {
+    promises.push(
+      new Promise(async (resolve) => {
+        const DexPair = await locklift.factory.getDeployedContract(
+          'DexPair',
+          dexPairAddress,
+        );
+
+        const root = await DexPair.methods
+          .getRoot({ answerId: 0 })
+          .call({})
+          .then((r) => r.dex_root.toString());
+
+        if (root === DEX_ROOT_ADDRESS) {
+          const roots = await DexPair.methods
+            .getTokenRoots({ answerId: 0 })
+            .call();
+
+          console.log(
+            `DexPair ${dexPairAddress}, left = ${roots.left}, right = ${roots.right}, lp = ${roots.lp}`,
+          );
+
+          resolve({
+            dexPair: dexPairAddress,
+            left: roots.left,
+            right: roots.right,
+            lp: roots.lp,
+          });
+        }
+      }),
+    );
+  }
+
+  const pairs = await Promise.all(promises);
+
+  console.log(`Export took ${(Date.now() - start) / 1000} seconds`);
+
+  writeFileSync(
+    './v2/scripts/prod/dex_pairs.json',
+    JSON.stringify(pairs, null, 2),
+  );
 }
 
 exportDexPairs()
-    .then(() => process.exit(0))
-    .catch(e => {
-      console.log(e);
-      process.exit(1);
-    });
-
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
