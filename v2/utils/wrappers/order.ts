c18cf88fa89520f3e71da2152dd431667f33bd51
andrey.sudov <andrey.sudov@latoken.com>
2023-02-07 19:07:01 +0300

    - Add wrappers (Order, Factory, Root, Wallet) - edit log utils

---
+++ v2/utils/wrappers/order.ts
@@ -0,0 +1,137 @@
+import {Address, Contract, Signer} from "locklift";
+import {FactorySource} from "../../../build/factorySource";
+import {Account} from 'locklift/everscale-client'
+import BigNumber from "bignumber.js";
+const {toNano} = locklift.utils;
+
+
+export class OrderWrapper {
+    public contract: Contract<FactorySource["Order"]>;
+    public _owner: Account | null;
+    public address: Address;
+
+    constructor(order_contract: Contract<FactorySource["Order"]>, order_owner: Account | null) {
+        this.contract = order_contract;
+        this._owner = order_owner;
+        this.address = this.contract.address;
+    }
+
+    static async from_addr(addr: Address, owner: Account | null) {
+        const order = await locklift.factory.getDeployedContract('Order', addr);
+        return new OrderWrapper(order, owner);
+    }
+
+    async feeParams() {
+        return await this.contract.methods.getFeeParams({answerId: 0}).call();
+    }
+
+    async status() {
+        return await this.contract.methods.currentStatus({answerId: 0}).call();
+    }
+
+    async expectedSpendAmount(amount: string | number) {
+        return (await this.contract.methods.getExpectedSpendAmount({answerId: 0, amount: amount}).call()).value0;
+    }
+
+    async expectedSpendAmountOfMatching(amount: string | number) {
+        return (await this.contract.methods.getExpectedSpendAmountOfMatching({answerId: 0, amount: amount}).call()).value0;
+    }
+
+    async buildPayload(
+        callbackId: number,
+        deployWalletValue: number
+        ) {
+        return this.contract.methods.buildPayload({callbackId: callbackId, deployWalletValue: locklift.utils.toNano(deployWalletValue)}).call()
+    }
+
+    async swap(
+        callbackId: number,
+        deployWalletValue: number,
+        trace: boolean = false,
+        from: Address
+    ) {
+
+        const owner = this._owner as Account;
+        if (trace){
+
+            return await locklift.tracing.trace(this.contract.methods.swap({
+                callbackId: 1,
+                deployWalletValue: locklift.utils.toNano(0.1)
+            }).send({
+                amount: locklift.utils.toNano(6), from: from
+            }), {allowedCodes: {compute: [60]}})
+        } else {
+            return await this.contract.methods.swap({
+                callbackId: 1,
+                deployWalletValue: locklift.utils.toNano(0.1)
+            }).send({
+                amount: locklift.utils.toNano(6), from: from
+            })
+        }
+    }
+    async proxyTokensTransfer(
+        _tokenWallet: Address,
+        _gasValue: number,
+        _amount: string,
+        _recipient: Address,
+        _deployWalletValue: number,
+        _remainingGasTo: Address,
+        _notify: boolean,
+        _payload: string = 'te6ccgEBAQEAAgAAAA==',
+        trace: boolean = false,
+        signer: Signer
+        ) {
+        if (trace){
+            return await locklift.tracing.trace(this.contract.methods.proxyTokensTransfer({
+                _tokenWallet: _tokenWallet,
+                _gasValue: locklift.utils.toNano(_gasValue),
+                _amount: _amount,
+                _recipient: _recipient,
+                _deployWalletValue: locklift.utils.toNano(_deployWalletValue),
+                _remainingGasTo: _remainingGasTo,
+                _notify: _notify,
+                _payload: _payload
+            }).sendExternal({publicKey: signer.publicKey}));
+        } else {
+                return await this.contract.methods.proxyTokensTransfer({
+                _tokenWallet: _tokenWallet,
+                _gasValue: locklift.utils.toNano(_gasValue),
+                _amount: _amount,
+                _recipient: _recipient,
+                _deployWalletValue: locklift.utils.toNano(_deployWalletValue),
+                _remainingGasTo: _remainingGasTo,
+                _notify: _notify,
+                _payload: _payload
+            }).sendExternal({publicKey: signer.publicKey});
+        }
+    }
+
+    async matching(
+        callbackId: number,
+        deployWalletValue: number,
+        limitOrder: Address,
+        from: Address,
+        trace: boolean
+    ) {
+        if (trace){
+            return await locklift.tracing.trace(
+                this.contract.methods.matching({
+                callbackId: callbackId,
+                deployWalletValue: locklift.utils.toNano(deployWalletValue),
+                limitOrder: limitOrder
+            }).send({
+                amount: locklift.utils.toNano(10), from: from
+            })
+            )
+        } else {
+            return await
+                this.contract.methods.matching({
+                callbackId: callbackId,
+                deployWalletValue: locklift.utils.toNano(deployWalletValue),
+                limitOrder: limitOrder
+            }).send({
+                amount: locklift.utils.toNano(10), from: from
+            })
+        }
+    }
+}
---
+++ v2/utils/wrappers/order_factory.ts
@@ -0,0 +1,198 @@
+import {Address, Contract, Signer, zeroAddress} from "locklift";
+import {FactorySource} from "../../../build/factorySource";
+import {Account} from 'locklift/everscale-client'
+import BigNumber from "bignumber.js";
+const {toNano} = locklift.utils;
+
+
+export class OrderFactory {
+    public contract: Contract<FactorySource["OrderFactory"]>;
+    public _owner: Account | null;
+    public address: Address;
+
+    constructor(order_contract: Contract<FactorySource["OrderFactory"]>, order_owner: Account | null) {
+        this.contract = order_contract;
+        this._owner = order_owner;
+        this.address = this.contract.address;
+    }
+
+    static async from_addr(addr: Address, owner: Account | null) {
+        const factory = await locklift.factory.getDeployedContract('Order', addr);
+        return new OrderFactory(factory, owner);
+    }
+
+    async feeParams() {
+        return await this.contract.methods.getFeeParams({answerId: 0}).call();
+    }
+
+    async owner() {
+        return await this.contract.methods.getOwner({answerId: 0}).call();
+    }
+
+    async setFeeParams(
+        feeNumerator: string | number,
+        feeDenominator: string | number,
+        matchingNumerator: string | number,
+        matchingDenominator: string | number,
+        beneficiary: Address = zeroAddress,
+        trace: boolean = false
+    ) {
+      const owner = this._owner as Account;
+      if (trace){
+        return await locklift.tracing.trace(this.contract.methods.setFeeParams({
+            params:
+            {
+              numerator: feeNumerator,
+              denominator: feeDenominator,
+              matchingNumerator: matchingNumerator,
+              matchingDenominator: matchingDenominator,
+              beneficiary: beneficiary
+            }
+          }).send({
+            amount: locklift.utils.toNano(5),
+            from: owner.address
+            }))
+      } else {
+          return await this.contract.methods.setFeeParams({
+            params:
+            {
+              numerator: feeNumerator,
+              denominator: feeDenominator,
+              matchingNumerator: matchingNumerator,
+              matchingDenominator: matchingDenominator,
+              beneficiary: beneficiary
+            }
+          }).send({
+            amount: locklift.utils.toNano(5),
+            from: owner.address
+            })
+      }
+
+    }
+
+    async setRootFeeParams(
+        root: Address,
+        feeNumerator: string | number,
+        feeDenominator: string | number,
+        matchingNumerator: string | number,
+        matchingDenominator: string | number,
+        beneficiary: Address = zeroAddress,
+        trace: boolean = false
+    ) {
+      const owner = this._owner as Account;
+      if (trace){
+        return await locklift.tracing.trace(this.contract.methods.setRootFeeParams({
+            root: root,
+            params:
+            {
+              numerator: feeNumerator,
+              denominator: feeDenominator,
+              matchingNumerator: matchingNumerator,
+              matchingDenominator: matchingDenominator,
+              beneficiary: beneficiary
+            }
+          }).send({
+            amount: locklift.utils.toNano(5),
+            from: owner.address
+            }))
+      } else {
+          return await this.contract.methods.setRootFeeParams({
+            root: root,
+            params:
+            {
+              numerator: feeNumerator,
+              denominator: feeDenominator,
+              matchingNumerator: matchingNumerator,
+              matchingDenominator: matchingDenominator,
+              beneficiary: beneficiary
+            }
+          }).send({
+            amount: locklift.utils.toNano(5),
+            from: owner.address
+            })
+      }
+
+    }
+
+    async withdrawFee(
+        amount: string | number,
+        recipient: Address,
+        sendGasTo: Address,
+        tokenWallet: Address,
+        deployWalletValue: number,
+        trace: boolean = false
+    ) {
+        const owner = this._owner as Account;
+
+        if (trace){
+            return await locklift.tracing.trace(this.contract.methods.withdrawFee({
+                amount: amount,
+                recipient: recipient,
+                sendGasTo: sendGasTo,
+                tokenWallet: tokenWallet,
+                deployWalletValue: locklift.utils.toNano(deployWalletValue)
+            }).send({amount: locklift.utils.toNano(2), from: owner.address}),{allowedCodes: {compute: [60]}})
+
+        } else {
+            return await this.contract.methods.withdrawFee({
+                amount: amount,
+                recipient: recipient,
+                sendGasTo: sendGasTo,
+                tokenWallet: tokenWallet,
+                deployWalletValue: locklift.utils.toNano(deployWalletValue)
+            }).send({amount: locklift.utils.toNano(2), from: owner.address}),{allowedCodes: {compute: [60]}}
+
+        }
+
+    }
+
+    async setOrderCode(
+        code: string
+    ) {
+        const owner = this._owner as Account;
+        return await locklift.tracing.trace(this.contract.methods.setOrderCode({_orderCode: code})
+        .send({amount: locklift.utils.toNano(1.1), from: owner.address}))
+
+    }
+    async updateOrder(
+        order: Address
+    ) {
+        const owner = this._owner as Account;
+        return await locklift.tracing.trace(this.contract.methods.upgradeOrder({order: order})
+        .send({amount: locklift.utils.toNano(1.1), from: owner.address}))
+    }
+
+        async setOrderRootCode(
+        code: string
+    ) {
+        const owner = this._owner as Account;
+        return await locklift.tracing.trace(this.contract.methods.setOrderRootCode({_orderRootCode: code})
+        .send({amount: locklift.utils.toNano(1.1), from: owner.address}))
+
+    }
+    async upgradeOrderRoot(
+        root: Address
+    ) {
+        const owner = this._owner as Account;
+        return await locklift.tracing.trace(this.contract.methods.upgradeOrderRoot({orderAddress: root})
+        .send({amount: locklift.utils.toNano(1.1), from: owner.address}))
+    }
+
+    async upgrade(
+        newCode: string,
+        sendGasTo: Address,
+        newVersion: string | number
+    ) {
+        const owner = this._owner as Account;
+        return await locklift.tracing.trace(this.contract.methods.upgrade(
+            {
+                newCode: newCode,
+                sendGasTo: sendGasTo,
+                newVersion: newVersion
+            })
+        .send({amount: locklift.utils.toNano(1.1), from: owner.address}))
+
+    }
+
+
+}
