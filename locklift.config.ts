ab056982b2bf12f8a8d690b9edfd35192d0d3f27
Aleksandr Hramcov <aleksandr.hramcov@gmail.com>
2023-06-13 15:43:29 +0200

    merge fixes + update pragma

--- locklift.config.ts
+++ locklift.config.ts
@@ -1,11 +1,10 @@
 import { LockliftConfig, lockliftChai } from 'locklift';
-import { GiverWallet, SimpleGiver, TestnetGiver } from "./giverSettings";
 import { FactorySource } from './build/factorySource';
+import 'locklift-verifier';
 
-import "locklift-deploy";
-import { Deployments } from "locklift-deploy";
+import { Deployments } from 'locklift-deploy';
 
-import chai from "chai";
+import chai from 'chai';
 chai.use(lockliftChai);
 
 declare global {
@@ -14,8 +13,9 @@ declare global {
   const locklift: import('locklift').Locklift<FactorySource>;
 }
 
-declare module "locklift" {
-  //@ts-ignore
+declare module 'locklift' {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   export interface Locklift {
     deployments: Deployments<FactorySource>;
   }
@@ -28,22 +28,24 @@ const LOCAL_NETWORK_ENDPOINT = 'http://localhost:80/graphql';
 
 const config: LockliftConfig = {
   compiler: {
-    version: "0.62.0",
+    version: '0.62.0',
     externalContracts: {
-      'precompiled': [
-          'DexPlatform',
-          'OrderRootPlatform',
-          'OrderPlatform'
-      ],
+      precompiled: ['DexPlatform', 'OrderRootPlatform', 'OrderPlatform'],
       'node_modules/tip3/build': [
         'TokenRootUpgradeable',
         'TokenWalletUpgradeable',
         'TokenWalletPlatform',
       ],
-      'node_modules/ever-wever/everscale/build' : []
+      'node_modules/ever-wever/everscale/build': [],
     },
   },
   linker: { version: '0.15.48' },
+  verifier: {
+    verifierVersion: 'latest', // contract verifier binary, see https://github.com/broxus/everscan-verify/releases
+    apiKey: '',
+    secretKey: '',
+    // license: "AGPL-3.0-or-later", <- this is default value and can be overrided
+  },
   networks: {
     local: {
       connection: {
@@ -57,24 +59,24 @@ const config: LockliftConfig = {
         },
       },
       giver: {
-        giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
-        address: '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
+        address:
+          '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
         key: '172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3',
       },
       tracing: { endpoint: LOCAL_NETWORK_ENDPOINT },
       keys: {
-        phrase: 'action inject penalty envelope rabbit element slim tornado dinner pizza off blood',
+        phrase:
+          'action inject penalty envelope rabbit element slim tornado dinner pizza off blood',
         amount: 20,
       },
     },
     test: {
       // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
       connection: {
-        group: "testnet",
-        // @ts-ignore
-        type: "graphql",
+        id: 0,
+        group: 'testnet',
+        type: 'graphql',
         data: {
-          // @ts-ignore
           endpoints: [process.env.TESTNET_GQL_ENDPOINT],
           latencyDetectionInterval: 1000,
           local: false,
@@ -82,58 +84,31 @@ const config: LockliftConfig = {
       },
       // This giver is default local-node giverV2
       giver: {
-        // Check if you need provide custom giver
-        giverFactory: process.env.TESTNET_GIVER_TYPE == "Wallet" ?
-            (ever, keyPair, address) => new GiverWallet(ever, keyPair, address) :
-            (ever, keyPair, address) => new TestnetGiver(ever, keyPair, address),
-        address: process.env.TESTNET_GIVER_ADDRESS ?? "",
-        phrase: process.env.TESTNET_GIVER_SEED ?? "",
-        accountId: 0
+        address: process.env.TESTNET_GIVER_ADDRESS ?? '',
+        phrase: process.env.TESTNET_GIVER_SEED ?? '',
+        accountId: 0,
       },
       tracing: {
-        endpoint: process.env.TESTNET_GQL_ENDPOINT ?? ""
+        endpoint: process.env.TESTNET_GQL_ENDPOINT ?? '',
       },
 
       keys: {
-        phrase: process.env.TESTNET_SEED_PHRASE ?? "",
-        amount: 20
+        phrase: process.env.TESTNET_SEED_PHRASE ?? '',
+        amount: 20,
       },
     },
     main: {
-      connection: "mainnetJrpc",
+      connection: 'mainnetJrpc',
       giver: {
-        // Mainnet giver has the same abi as testnet one
-        giverFactory: process.env.MAIN_GIVER_TYPE == "Wallet" ?
-            (ever, keyPair, address) => new GiverWallet(ever, keyPair, address) :
-            (ever, keyPair, address) => new TestnetGiver(ever, keyPair, address),
-        address: process.env.MAIN_GIVER_ADDRESS ?? "",
-        phrase: process.env.MAIN_GIVER_SEED ?? "",
-        accountId: 0
-      },
-      tracing: {
-        endpoint: process.env.MAIN_GQL_ENDPOINT ?? ""
+        address: process.env.MAIN_GIVER_ADDRESS ?? '',
+        phrase: process.env.MAIN_GIVER_SEED ?? '',
+        accountId: 0,
       },
       keys: {
-        phrase: process.env.MAIN_SEED_PHRASE ?? "",
-        amount: 20
-      }
+        phrase: process.env.MAIN_SEED_PHRASE ?? '',
+        amount: 20,
+      },
     },
-    prod: {
-      connection: "mainnetJrpc",
-      giver: {
-        // Mainnet giver has the same abi as testnet one
-        giverFactory: (ever, keyPair, address) => new TestnetGiver(ever, keyPair, address),
-        address: "0:3bcef54ea5fe3e68ac31b17799cdea8b7cffd4da75b0b1a70b93a18b5c87f723",
-        key: process.env.MAIN_GIVER_KEY ?? "action inject penalty envelope rabbit element slim tornado dinner pizza off blood"
-      },
-      tracing: {
-        endpoint: process.env.MAIN_GQL_ENDPOINT ?? ""
-      },
-      keys: {
-        phrase: process.env.MAIN_SEED_PHRASE ?? "",
-        amount: 500
-      }
-    }
   },
   mocha: { timeout: 2000000 },
 };
