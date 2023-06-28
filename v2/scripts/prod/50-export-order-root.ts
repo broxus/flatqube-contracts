48d36ea09ae0906ce6b82a9fcc1d765440d40aa9
Aleksandr Hramcov <aleksandr.hramcov@gmail.com>
2023-06-28 15:35:04 +0200

    cleanup

--- v2/scripts/prod/50-export-order-root.ts
+++ v2/scripts/prod/50-export-order-root.ts
@@ -1,46 +1,50 @@
-import {writeFileSync} from "fs";
+import { writeFileSync } from 'fs';
 
-const ORDER_ROOT_CODE_HASH = '75e59d7ac15f9c12339fcd01998916ba9fc4ecc65ed7e557d3b1e3cd7e70938d';
-const ORDER_FACTORY_ADDRESS = '0:3c8d39684cabbb780ff77710b02923c59ea2be84e211b09c3258eef344d394a4';
+const ORDER_ROOT_CODE_HASH =
+  '75e59d7ac15f9c12339fcd01998916ba9fc4ecc65ed7e557d3b1e3cd7e70938d';
+const ORDER_FACTORY_ADDRESS =
+  '0:3c8d39684cabbb780ff77710b02923c59ea2be84e211b09c3258eef344d394a4';
 
 async function main() {
-    const orderRootsForUpdate = [];
-    let continuation = undefined;
-    let hasResults = true;
-    while (hasResults) {
-        const result: any = await locklift.provider.getAccountsByCodeHash({
-            codeHash: ORDER_ROOT_CODE_HASH,
-            continuation,
-            limit: 50,
+  const orderRootsForUpdate = [];
+  let continuation = undefined;
+  let hasResults = true;
+  while (hasResults) {
+    const result: any = await locklift.provider.getAccountsByCodeHash({
+      codeHash: ORDER_ROOT_CODE_HASH,
+      continuation,
+      limit: 50,
+    });
+    continuation = result.continuation;
+    hasResults = result.accounts.length === 50;
+    for (const orderRootAddress of result.accounts) {
+      const OrderRoot = await locklift.factory.getDeployedContract(
+        'OrderRoot',
+        orderRootAddress,
+      );
+
+      const factoryAddress = (
+        await OrderRoot.methods.getFactory({ answerId: 0 }).call({})
+      ).value0.toString();
+      if (factoryAddress == ORDER_FACTORY_ADDRESS) {
+        orderRootsForUpdate.push({
+          orderRoot: orderRootAddress,
         });
-        continuation = result.continuation;
-        hasResults = result.accounts.length === 50;
-        for (const orderRootAddress of result.accounts) {
-            const OrderRoot = await locklift.factory.getDeployedContract(
-                'OrderRoot',
-                orderRootAddress
-            );
-
-            const factoryAddress = (await OrderRoot.methods.getFactory({answerId: 0}).call({})).value0.toString();
-            if (factoryAddress == ORDER_FACTORY_ADDRESS) {
-                orderRootsForUpdate.push({
-                    orderRoot: orderRootAddress,
-                });
-            }
-        }
+      }
     }
+  }
 
-    console.log(`Count order roots: ${orderRootsForUpdate.length}`);
+  console.log(`Count order roots: ${orderRootsForUpdate.length}`);
 
-    writeFileSync(
-        './order_roots.json',
-        JSON.stringify(orderRootsForUpdate, null, 2),
-    );
+  writeFileSync(
+    './order_roots.json',
+    JSON.stringify(orderRootsForUpdate, null, 2),
+  );
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((e) => {
-        console.log(e);
-        process.exit(1);
-});
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
--- v2/scripts/update-orders.ts
+++ v2/scripts/update-orders.ts
@@ -1,76 +1,77 @@
-import {Address, toNano, WalletTypes} from "locklift";
+import { Address, toNano, WalletTypes } from 'locklift';
 import { yellowBright } from 'chalk';
-import orders from 'orders.json';
+import orders from '../../orders.json';
 
-const ORDER_FACTORY_ADDRESS = "0:3c8d39684cabbb780ff77710b02923c59ea2be84e211b09c3258eef344d394a4";
+const ORDER_FACTORY_ADDRESS =
+  '0:3c8d39684cabbb780ff77710b02923c59ea2be84e211b09c3258eef344d394a4';
 
 const chunkify = <T>(arr: T[], size: number): T[][] =>
-    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-        arr.slice(i * size, i * size + size),
-    );
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
 
 async function main() {
-    const OrderFactory = await locklift.factory.getDeployedContract(
-        'OrderFactory',
-        new Address(ORDER_FACTORY_ADDRESS)
+  const OrderFactory = await locklift.factory.getDeployedContract(
+    'OrderFactory',
+    new Address(ORDER_FACTORY_ADDRESS),
+  );
+
+  const limitOrdersManager = await OrderFactory.methods
+    .getManager({ answerId: 0 })
+    .call()
+    .then((m) => m.value0);
+
+  const manager = await locklift.factory.accounts.addExistingAccount({
+    type: WalletTypes.EverWallet,
+    address: limitOrdersManager,
+  });
+
+  console.log('DexRoot:' + OrderFactory.address);
+  console.log('Manager:' + manager.address);
+
+  const params = orders.map((a) => ({
+    orderAddress: a.order,
+  }));
+
+  for (const chunk of chunkify(params, 10)) {
+    const { traceTree } = await locklift.tracing.trace(
+      OrderFactory.methods
+        .upgradeOrder({
+          listOrders: chunk.map((a) => new Address(a.orderAddress)),
+        })
+        .send({
+          from: manager.address,
+          amount: toNano(chunk.length * 0.5 + 1),
+        }),
     );
 
-    const limitOrdersManager = await OrderFactory.methods
-        .getManager({answerId: 0})
-        .call()
-        .then((m)=>m.value0);
+    for (const orders of chunk) {
+      const Order = locklift.factory.getDeployedContract(
+        'Order',
+        new Address(orders.orderAddress),
+      );
 
-    const manager = await locklift.factory.accounts.addExistingAccount({
-        type: WalletTypes.EverWallet,
-        address: limitOrdersManager
-    });
+      const events = traceTree.findEventsForContract({
+        contract: Order,
+        name: 'OrderCodeUpgraded' as const,
+      });
 
-    console.log('DexRoot:' + OrderFactory.address);
-    console.log('Manager:' + manager.address);
-
-    const params = orders.map((a) => ({
-        orderAddress: a.order,
-    }));
-
-    for (const chunk of chunkify(params, 1000)) {
-        const {traceTree} = await locklift.tracing.trace(
-            OrderFactory.methods
-                .upgradeOrder({
-                    listOrders: chunk.map((a) => new Address(a.orderAddress)),
-                })
-                .send({
-                    from: manager.address,
-                    amount: toNano(chunk.length * 0.5 + 0.1)
-                }),
+      if (events.length > 0) {
+        console.log(
+          `Order ${orders.orderAddress} upgraded. Current version: ${events[0].newVersion}`,
         );
-
-        for (const orders of chunk) {
-            const Order = locklift.factory.getDeployedContract(
-                'Order',
-                new Address(orders.orderAddress),
-            );
-
-            const events = traceTree.findEventsForContract({
-                contract: Order,
-                name: 'OrderCodeUpgraded' as const,
-            });
-
-            if (events.length > 0) {
-                console.log(
-                    `Order ${orders.orderAddress} upgraded. Current version: ${events[0].newVersion}`,
-                );
-            } else {
-                console.log(
-                    yellowBright(`Order ${orders.orderAddress} wasn't upgraded`),
-                );
-            }
-        }
+      } else {
+        console.log(
+          yellowBright(`Order ${orders.orderAddress} wasn't upgraded`),
+        );
+      }
     }
+  }
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(e => {
-        console.log(e);
-        process.exit(1);
-});
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
