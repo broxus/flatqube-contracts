afd3d21fcc6ca032e0229d7b631c8a7ba5db216f
matveyb <matvey.b2022@gmail.com>
2023-08-02 14:44:02 +0200

    deploy tokens update

--- deploy/tokensDeploy/10-deploy-tokens.ts
+++ deploy/tokensDeploy/10-deploy-tokens.ts
@@ -1,5 +1,6 @@
 import { toNano, zeroAddress } from 'locklift';
-export const TOKENS_N = 5;
+export const TOKENS_N = 1;
+export const TOKENS_DECIMALS = [6, 9, 18];
 
 export default async () => {
   const signer = await locklift.keystore.getSigner('0');
@@ -11,37 +12,39 @@ export default async () => {
     'TokenWalletPlatform',
   );
 
-  for (let i = 0; i < TOKENS_N; i++) {
-    await locklift.transactions.waitFinalized(
-      locklift.deployments.deploy({
-        deployConfig: {
-          contract: 'TokenRootUpgradeable',
-          publicKey: signer.publicKey,
-          initParams: {
-            randomNonce_: locklift.utils.getRandomNonce(),
-            deployer_: zeroAddress,
-            name_: `TST-${i}`,
-            symbol_: `TST-${i}`,
-            decimals_: 9,
-            walletCode_: walletArtifacts.code,
-            rootOwner_: owner.address,
-            platformCode_: platformArtifacts.code,
+  for (let i = 0; i < TOKENS_DECIMALS.length; i++) {
+    for (let k = 0; k < TOKENS_N; k++) {
+      await locklift.transactions.waitFinalized(
+        locklift.deployments.deploy({
+          deployConfig: {
+            contract: 'TokenRootUpgradeable',
+            publicKey: signer.publicKey,
+            initParams: {
+              randomNonce_: locklift.utils.getRandomNonce(),
+              deployer_: zeroAddress,
+              name_: `TST-${i}-${k}`,
+              symbol_: `TST-${i}-${k}`,
+              decimals_: 9,
+              walletCode_: walletArtifacts.code,
+              rootOwner_: owner.address,
+              platformCode_: platformArtifacts.code,
+            },
+            constructorParams: {
+              initialSupplyTo: owner.address,
+              initialSupply: 10 ** 18,
+              deployWalletValue: toNano(0.5),
+              mintDisabled: false,
+              burnByRootDisabled: true,
+              burnPaused: true,
+              remainingGasTo: owner.address,
+            },
+            value: toNano(10),
           },
-          constructorParams: {
-            initialSupplyTo: owner.address,
-            initialSupply: 10 ** 18,
-            deployWalletValue: toNano(0.5),
-            mintDisabled: false,
-            burnByRootDisabled: true,
-            burnPaused: true,
-            remainingGasTo: owner.address,
-          },
-          value: toNano(10),
-        },
-        deploymentName: `token-${i}`,
-        enableLogs: true,
-      }),
-    );
+          deploymentName: `token-${i}-${k}`,
+          enableLogs: true,
+        }),
+      );
+    }
   }
 };
 
