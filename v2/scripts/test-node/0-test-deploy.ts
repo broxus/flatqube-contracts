ee0ecff27118ca490ab75bdd601782fa5336283d
matveyb <matvey.b2022@gmail.com>
2023-08-08 16:41:17 +0200

    deploy fix

--- v2/scripts/test-node/0-test-deploy.ts
+++ v2/scripts/test-node/0-test-deploy.ts
@@ -22,7 +22,7 @@ async function main() {
   //     "common-accounts",
   //     "token-factory",
   //     "dex-root",
-  //     "dex-account",
+  //     "dex-accounts",
   //     "wever",
   //     "ever-wever-tip3",
   //     "wever-wallets",
