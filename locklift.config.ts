a28ddf02d1345d775749ae8fd8dc133fdd3b729d
Alexander Kunekov <fairyfromalfeya@gmail.com>
2023-08-09 18:41:21 +0300

    build: added new wever vault contract to config

--- locklift.config.ts
+++ locklift.config.ts
@@ -36,6 +36,10 @@ const config: LockliftConfig = {
         'TokenWalletPlatform',
       ],
       'node_modules/ever-wever/everscale/build': [],
+      'node_modules/@broxus/wever/build': [
+        'VaultTokenRoot_V1',
+        'VaultTokenWallet_V1',
+      ],
     },
   },
   linker: { version: '0.15.48' },
@@ -59,13 +63,13 @@ const config: LockliftConfig = {
       },
       giver: {
         address:
-            '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
+          '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
         key: '172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3',
       },
       tracing: { endpoint: LOCAL_NETWORK_ENDPOINT },
       keys: {
         phrase:
-            'action inject penalty envelope rabbit element slim tornado dinner pizza off blood',
+          'action inject penalty envelope rabbit element slim tornado dinner pizza off blood',
         amount: 20,
       },
     },
