62214ddf5a356c63bf89ac67dbe9e5631e1dbe7a
Елена Яковлева <el.iakovv@gmail.com>
2023-10-16 14:50:17 +0200

    update test_prepare_dex scripts

--- prepare_test_dex_venom.sh
+++ prepare_test_dex_venom.sh
@@ -1,26 +1,22 @@
-export PARAMS="--disable-build --network main"
+export PARAMS="--disable-build --network venom_testnet"
 
 npx locklift deploy $PARAMS --tags owner-account token-factory wever dex-gas-values dex-root dex-accounts --force
 
 npx locklift run $PARAMS --script scripts/2-deploy-test-tokens.ts --tokens='["foo","bar","qwe","tst"]'
 
-npx locklift run $PARAMS --script scripts/5-deploy-test-pair.ts --pairs='[["tst","foo"]]' --contract_name='DexPair'
-npx locklift run $PARAMS --script scripts/5-deploy-test-pair.ts --pairs='[["tst","bar"]]' --contract_name='DexPair'
-npx locklift run $PARAMS --script scripts/5-deploy-test-pool.ts --pools='[["foo", "bar", "qwe"]]' --contract_name='DexStablePool'
-
-npx locklift run $PARAMS --script scripts/35-upgrade-pair.ts --roots='["tst", "foo"]' --old_contract_name='DexPair' --new_contract_name='DexStablePair' --pool_type=2
-
-npx locklift run $PARAMS --script scripts/90-set-test-params.ts
+npx locklift run $PARAMS --script scripts/5-deploy-test-pool.ts --pairs='[["foo","tst"]]' --contract_name='DexStablePool'
+npx locklift run $PARAMS --script scripts/5-deploy-test-pool.ts --pairs='[["foo","bar"]]' --contract_name='DexStablePool'
+npx locklift run $PARAMS --script scripts/5-deploy-test-pool.ts --pairs='[["foo","qwe"]]' --contract_name='DexStablePool'
+npx locklift run $PARAMS --script scripts/5-deploy-test-pair.ts --pairs='[["foo","bar"]]' --contract_name='DexPair'
 
 npx locklift run $PARAMS --script scripts/98-mint-test-tokens-to.ts \
---to='0:7a43a08e77dcc2bd7ce2f5f6798dbb84af9c8443e8bfb60c27e125033fef1760' \
+--to='0:d5d979b24d687f2f16859d5c7a5e5167c2347232a92e189c802f74dc9fb642a2' \
 --mints='[{"account":2,"amount":200000000,"token":"foo"}, {"account":2,"amount":200000000,"token":"bar"}, {"account":2,"amount":200000000,"token":"tst"}, {"account":2,"amount":200000000,"token":"qwe"}]'
 
 npx locklift run $PARAMS --script scripts/98-mint-test-tokens-to.ts \
---to='0:33478651d9c7b44c1b45c2dfe85edf7a5d24692f5222f0a25c176b1abfd95e51' \
+--to='0:d5d979b24d687f2f16859d5c7a5e5167c2347232a92e189c802f74dc9fb642a2' \
 --mints='[{"account":2,"amount":200000000,"token":"foo"}, {"account":2,"amount":200000000,"token":"bar"}, {"account":2,"amount":200000000,"token":"tst"}, {"account":2,"amount":200000000,"token":"qwe"}]'
 
 npx locklift run $PARAMS --script scripts/5-deploy-test-pair.ts --pairs='[["wever","foo"]]' --contract_name='DexPair'
 
 npx locklift run $PARAMS --script scripts/10-set-wever-root-to-token-vault.ts --config locklift.config.ts
-
